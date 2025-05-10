7c9dd79b50541f4e54407c30fa0817edd222d8b9
kunalpro379 <2022.kunal.patil@ves.ac.in>
2025-05-10 23:42:07 +0530

    build error fixed

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import markdownPlugin from './src/vite-plugins/markdown';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    tsconfigPaths(),
     markdownPlugin()
   ],
   resolve: {
